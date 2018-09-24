24ad09049c19195b11e889cf26d32b616a1e2848
Paulius Juknis <paulius.juknis@gmail.com>
2018-09-24 15:07:07 +0300

    adding project starter files from github

--- simple-product-manager/src/app/app.component.ts
+++ simple-product-manager/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'pm-app',
+    template: `
+        <h1>Angular2: Getting Started</h1>
+    `
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent { }
