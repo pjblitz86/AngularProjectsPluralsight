cdaac2bb39c1ebc23af3c6b74be23ee9dfaa470b
Paulius Juknis <paulius.juknis@gmail.com>
2018-09-26 10:20:37 +0300

    implemented a search filter for product-list component

--- simple-product-manager/src/app/products/product-list.component.ts
+++ simple-product-manager/src/app/products/product-list.component.ts
@@ -11,7 +11,18 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  filteredProducts: IProduct[];
+
   products: IProduct[] = [
     {
       productId: 2,
@@ -35,7 +46,19 @@ export class ProductListComponent implements OnInit {
     }
   ];
 
+  constructor() {
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
+  }
+
   ngOnInit(): void {}
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter(
+      (product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
